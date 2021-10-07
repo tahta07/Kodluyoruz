1427ea11004109885f932420686146957c5ca5ef
Erol YUNUS <erol.yunus@ozgvn.com>
2021-10-07 14:11:01 +0300

    functions created and complated

--- Patika-dev-odev1/js/app.js
+++ Patika-dev-odev1/js/app.js
@@ -1,14 +1,28 @@
+// DOM elementlerinin seçilmesi ve değişkenlere atanması
+const myName = document.getElementById("myName")
+const myClock = document.getElementById("myClock")
+
+
+// Sayfa yüklenirken Kullanıcıdan İsmini isteyip DOM'a yazan fonksiyon
 function askName(){
     const getName = prompt("İsminizi Giriniz")
-    document.getElementById("myName").innerText = getName
+    myName.innerText= getName
 }
 
 function showTime(){
+
+// Tarih ve gün bilgilerini alır
     let date = new Date()
-let h = date.getHours()
-let m = date.getMinutes()
-let s = date.getSeconds()
-let day = date.getDay()
+    let h = date.getHours()
+    let m = date.getMinutes()
+    let s = date.getSeconds()
+    let day = date.getDay()
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+//Gün bilgisine göre hangi gün olduğunu belirler
 
 switch (day){
     case 1:
@@ -32,16 +46,16 @@ switch (day){
     case 0:
         day= "Pazar"
         break
+}
+
+writeTime(h,m,s,day)
 
 }
 
-h = h < 10 ? "0" + h : h;
-m = m < 10 ? "0" + m : m;
-s = s < 10 ? "0" + s : s;
-
-
-let time = `${h}:${m}:${s} ${day}`
-document.getElementById("myClock").innerText = time;
+//DOM'a Saati ve Günü Yazar
+function writeTime(h,m,s,day){
+    let time = `${h}:${m}:${s} ${day}`
+    myClock.innerText = time;
 }
 
 askName()
--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 Kodluyoruz Front-End Bootcamp Ödevleri bu repository içinde yer alacaktır
 
 
+
+
 # Kodluyoruz Ödevleri
     
 
 
 # Patika.dev Ödevleri
 
-- Javascript
+- Javascript Patikası
     - <a href = "https://github.com/tahta07/Kodluyoruz/tree/main/Patika-dev-odev1" >Ödev 1 </a> 
 
